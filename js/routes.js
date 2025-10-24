bbede3b527cfc2e1b9a10ddb7175ef69cb6e899b
kitemy <misser.ilyes2020@gmail.com>
2025-10-24 15:51:42 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,7 @@
-// js/routes.js
 import List from "./pages/List.js";
+import Roulette from "./pages/Roulette.js";
 
 export default [
-  { path: "/", component: List }
+  { path: "/", component: List },
+  { path: "/roulette", component: Roulette }
 ];
