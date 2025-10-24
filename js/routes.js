ef486a26aa9bc708c441266058e69939cdd71403
kitemy <misser.ilyes2020@gmail.com>
2025-10-24 15:49:48 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,8 +1,6 @@
-import List from './pages/List.js';
-import Leaderboard from './pages/Leaderboard.js';
-import Roulette from './pages/Roulette.js';
+// js/routes.js
+import List from "./pages/List.js";
+
 export default [
-    { path: '/', component: List },
-    // { path: '/leaderboard', component: Leaderboard },
-    // { path: '/roulette', component: Roulette }
+  { path: "/", component: List }
 ];
