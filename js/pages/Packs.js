403d6777265a1cad3ebd1674feae910fe8a620dc
kitemy <misser.ilyes2020@gmail.com>
2025-10-24 11:53:34 +0200

    Update Packs.js

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -1,3 +1,7 @@
+import Colorblind from "../../data/Colorblind.json";
+import Champions-Road from "../../data/Champions-Road.json";
+import My-Spike-is-Laggy from "../../data/My-Spike-is-Laggy.json";
+
 export default {
   name: "Packs",
   data() {
@@ -5,79 +9,42 @@ export default {
       packs: [
         {
           name: "The Former Top 1's",
-          levelFileNames: ["Colorblind", "Champions-Road", "My-Spike-is-Laggy"],
+          levels: [Colorblind, Champions-Road, My-Spike-is-Laggy],
           bonusPoints: 150,
         },
       ],
-      levelsData: {},
-      loading: true,
     };
   },
-  async created() {
-    const allFiles = Array.from(new Set(this.packs.flatMap(p => p.levelFileNames)));
-    for (const file of allFiles) {
-      try {
-        const res = await fetch(`/data/${file}.json`);
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        const data = await res.json();
-        this.levelsData[file] = data;
-        console.log(`Loaded ${file}.json`, data);
-      } catch (e) {
-        console.error(`Error loading ${file}.json`, e);
-      }
-    }
-    this.loading = false;
-  },
   methods: {
-    getLevel(file) {
-      return this.levelsData[file] || {};
-    },
     getEmbedUrl(url) {
       if (!url) return null;
-      // Support YouTube URL formats
-      let videoId = null;
       const short = url.match(/youtu\.be\/([A-Za-z0-9_-]{11})/);
       const long = url.match(/v=([A-Za-z0-9_-]{11})/);
       const embed = url.match(/embed\/([A-Za-z0-9_-]{11})/);
-      if (short) videoId = short[1];
-      else if (long) videoId = long[1];
-      else if (embed) videoId = embed[1];
-      if (!videoId) return null;
-      return `https://www.youtube.com/embed/${videoId}`;
+      const id = short?.[1] || long?.[1] || embed?.[1];
+      return id ? `https://www.youtube.com/embed/${id}` : null;
     },
   },
   template: `
     <div id="tab-packs" class="packs-grid">
-      <div v-if="loading">Loading packs...</div>
-      <div v-else>
-        <div v-for="pack in packs" :key="pack.name" class="pack-card">
-          <h3>{{ pack.name }}</h3>
-          <ul>
-            <li v-for="file in pack.levelFileNames" :key="file">
-              {{ getLevel(file).name || file }}
-            </li>
-          </ul>
-
-          <div class="videos">
-            <div
-              v-for="file in pack.levelFileNames"
-              :key="file + '-vid'"
-              v-if="getEmbedUrl(getLevel(file).link)"
-              class="video-frame"
-            >
-              <iframe
-                width="100%"
-                height="180"
-                :src="getEmbedUrl(getLevel(file).link)"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </div>
+      <div v-for="pack in packs" :key="pack.name" class="pack-card">
+        <h3>{{ pack.name }}</h3>
+        <ul>
+          <li v-for="lvl in pack.levels" :key="lvl.name">{{ lvl.name }}</li>
+        </ul>
+        <div class="videos">
+          <div v-for="lvl in pack.levels" :key="lvl.name + '-vid'" v-if="getEmbedUrl(lvl.link)" class="video-frame">
+            <iframe
+              width="100%"
+              height="180"
+              :src="getEmbedUrl(lvl.link)"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
           </div>
-
-          <p class="bonus">Bonus: +{{ pack.bonusPoints }} pts</p>
         </div>
+        <p class="bonus">Bonus: +{{ pack.bonusPoints }} pts</p>
       </div>
     </div>
   `,
