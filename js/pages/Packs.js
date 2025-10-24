384f085a0d60a8b1d95b0d0fb1fe2a9c5e2959f9
kitemy <misser.ilyes2020@gmail.com>
2025-10-24 18:43:04 +0200

    Update Packs.js

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -6,7 +6,7 @@ export default {
       packs: [
         {
           name: "The Former Top 1's",
-          levels: ["Colorblind", "Champions Road", "My Spike is Laggy"],
+          levels: ["Colorblind", "Champions-Road", "My-Spike-is-Laggy"],
           bonusPoints: 150
         }
       ],
