abce2cdc7aabe881b805dc113ae8ef7e67b0137e
kitemy <misser.ilyes2020@gmail.com>
2025-10-25 22:45:00 +0200

    Update Packs.js

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -11,7 +11,7 @@ export default {
         {
           name: "TBL Favorites I",
           bonusPoints: 75,
-          levels: ["LgTn", "Fleur-de-lotus", "Arc-en-ciel", "Skill-control", "Croissant"],
+          levels: ["LgTn", "Fleur-de-lotus", "Arc-en-Ciel", "Skill-Control", "Croissant"],
         }
       ],
       loading: true,
