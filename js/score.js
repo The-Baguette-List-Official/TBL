9fe3fab588973b9d4252824af344bc0a0c07a317
Ghouno <florytristan24@gmail.com>
2025-10-30 02:25:59 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 0;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 200) {
+    if (rank > 175) {
         return 0;
     }
     if (rank > 75 && percent < 100) {
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-25*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-25*Math.pow(rank-1, 0.4) + 175) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
