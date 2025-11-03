df275005398783f948b7c231e0d7938634842941
seth <133776076+Seth2881@users.noreply.github.com>
2025-11-03 21:53:47 +0100

    Update score calculation formula in score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-25*Math.pow(rank-1, 0.4) + 157) *
+    let score = (-25*Math.pow(rank-1, 0.4) + 158) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
